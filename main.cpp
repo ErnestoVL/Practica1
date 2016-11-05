0d053647dbfec9968ffb8a6caab59e3c58f8c3a5
ErnestoVL <ciberernes@hotmail.com>
2016-11-05 21:54:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,41 +4,69 @@
 #include <cctype>
 using namespace std;
 
-int solicitaAnyo(int& anyo) {
-	cout << "Indroduzca un año: " ;
+int solicitaAnyo(int& anyo, string msg) {
+	cout << msg ;
 	cin >> anyo;
-	while (anyo < 1900 || !cin.good()){
+	while (anyo < 1900){
+		cin.fail ();
 		cin.clear();
 		cin.sync();
 		cout << "ERROR: El año introducido no es válido" << endl;
-		cout << "Introduzca un año: ";
+		cout << msg;
 		cin >> anyo;
 	}
 	return anyo;
 }
 
-int solicitaMes(int& mes) {
-	cout << "Introduzca un mes: ";
+int solicitaMes(int& mes, string msg) {
+	cout << msg;
 	cin >> mes;
-	while (mes < 1 || mes > 12 || !cin.good()){
+	while (mes < 1 || mes > 12){
+		cin.fail ();
 		cin.clear();
 		cin.sync();
 		cout << "ERROR: El mes introducido no es válido" << endl;
-		cout << "Introduzca un mes: ";
+		cout << msg;
 		cin >> mes;
 	}
 	return mes;
 }
 
-void mostrar(int anyo, int mes) {
-	cout << anyo << endl;
-	cout << mes << endl;
+bool esBisiesto (int anyo){
+	return anyo%4==0 && anyo%100!=0 || anyo%400==0;
 }
 
-int main(int argc, char** args) {
-	int anyo, mes;
-	solicitaAnyo(anyo);
-	mostrar(anyo, mes);
+int solicitaDia(int anyo, int mes, int& dia, string msg){
+	cout <<msg;
+	cin >> dia;
+	if (dia<1 || dia>31){
+		solicitaDia (anyo, mes, dia, msg);
+	}
+	if (dia>30 && mes==4 || mes==6 || mes==9 || mes==11){
+		solicitaDia(anyo, mes, dia, msg);
+	}
+	if (dia>28 && mes==2 && !esBisiesto(anyo)){
+		solicitaDia(anyo, mes, dia, msg);
+	}
+	if (dia>29 && mes==2 && esBisiesto(anyo)){
+		solicitaDia(anyo, mes, dia, msg);
+	}
+	return dia;
+}
+
+
+
+void mostrar(int anyo, int mes, int dia){
+	cout <<dia <<"/" <<mes <<"/" <<anyo <<endl;
+}
+
+int main(int argc, char** args){
+	setlocale(LC_ALL, "spanish");
+	int anyo, mes, dia;
+	solicitaAnyo(anyo, "Introduzca un año: ");
+	solicitaMes(mes, "Introduzca un mes: ");
+	solicitaDia (anyo, mes, dia, "Introduzca un dia: ");
+	mostrar(anyo, mes, dia);
 	system("pause");
 	return 0;
 }
