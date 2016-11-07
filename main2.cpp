c7bc5de76e273d94b576d4441a41d01c791aaeb6
ErnestoVL <ciberernes@hotmail.com>
2016-11-08 00:45:32 +0100

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -40,37 +40,66 @@ int solicitaDia(int anyo, int mes, int& dia, string msg){
 	cout <<msg;
 	cin >> dia;
 	if (dia<1 || dia>31){
-		cout <<"ERROR: El dia introducido no es válido" <<endl;
 		solicitaDia(anyo, mes, dia, msg);
 	}
-	if (dia>30 && mes==4 || mes==6 || mes==9 || mes==11){
-		cout <<"ERROR: El dia introducido no es válido" <<endl;
-		solicitaDia(anyo, mes, dia, msg);
+	if ((mes==4 || mes==6 || mes==9 || mes==11) && (dia>30)){
+		solicitaDia(anyo, mes, dia, msg);	
 	}
 	if (dia>28 && mes==2 && !esBisiesto(anyo)){
-		cout <<"ERROR: El dia introducido no es válido" <<endl;
 		solicitaDia(anyo, mes, dia, msg);
 	}
 	if (dia>29 && mes==2 && esBisiesto(anyo)){
-		cout <<"ERROR: El dia introducido no es válido" <<endl;
 		solicitaDia(anyo, mes, dia, msg);
 	}
 	return dia;
 }
 
 
-
-void mostrar(int anyo, int mes, int dia){
-	cout <<dia <<"/" <<mes <<"/" <<anyo <<endl;
+int diasAnyosCompletos (int anyo, int numBisiestos){ //<-- dias entre 1/1/1900--1/1/aaaa
+	int numAnyos;
+	numAnyos = (anyo-numBisiestos)*365+numBisiestos;
+	return numAnyos;	
 }
+
+/*
+int diasEsteAnyo (int dia, int mes, int anyo){ //<-- dias entre 1/1/aaaa--dd/mm/aaaa
+	
+}
+*/
+
+int contarBisiestos (int anyoInicio, int anyoFinal, int anyo){ //<-- numero de bisiestos entre 1900--aaaa
+	int numBisiestos;
+	anyoInicio=1900;
+	anyoFinal=anyo;
+	numBisiestos = (anyoFinal-anyoInicio)/4; //<-- Dividir entre 4 pues es el periodo de los bisiestos
+	return numBisiestos;
+}
+
+long int diasTranscurridos(int mes, int anyo, int dia){ //<-- dias entre 1/1/1900--dd/mm/aaaa
+	int numDias;
+	numDias = dia+mes+anyo;
+	return numDias;
+}
+
+
+void mostrar(int anyo, int mes, int dia, int dif, int numBisiestos, int numAnyos){
+	cout <<dia <<"/" <<mes <<"/" <<anyo <<endl;
+	cout <<dif <<endl;
+	cout <<numBisiestos <<endl;
+	cout <<numAnyos <<endl;
+}
+
 
 int main(int argc, char** args){
 	setlocale(LC_ALL, "spanish");
-	int anyo, mes, dia;
+	int anyo, mes, dia, difM, difA, difD, difT, dif, anyoFinal, anyoInicio, numBisiestos, numAnyos;
 	solicitaAnyo(anyo, "Introduzca un año: ");
 	solicitaMes(mes, "Introduzca un mes: ");
 	solicitaDia (anyo, mes, dia, "Introduzca un dia: ");
-	mostrar(anyo, mes, dia);
+	numBisiestos = contarBisiestos(anyoInicio, anyoFinal, anyo);
+	numAnyos = diasAnyosCompletos(anyo, numBisiestos);
+	dif=diasTranscurridos(dia, mes, anyo);
+	mostrar(anyo, mes, dia,dif, numBisiestos, numAnyos);
 	system("pause");
 	return 0;
 }
