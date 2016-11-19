b0853cc057573c5a4f67d127441229a212b8280c
ErnestoVL <ciberernes@hotmail.com>
2016-11-19 22:11:22 +0100

    Update main(Hecho-en-Casa).cpp

--- Hecho-en-casa/main(Hecho-en-Casa).cpp
+++ Hecho-en-casa/main(Hecho-en-Casa).cpp
@@ -1,87 +1,90 @@
-/**********HECHO EN CASA**********/
+
 #include <iostream>
 #include <cmath>
 #include <cstring>
 using namespace std;
 
 
-int solicitaAnyo(int& anyo, string msg) {
-	cout << msg ;
+int solicitaAnyo(int& anyo) {
+	cout << "Introduce el año: ";
 	cin >> anyo;
 	while (anyo < 1900){
-		cin.fail ();
+		cin.fail();
 		cin.clear();
 		cin.sync();
 		cout << "ERROR: El año introducido no es válido" << endl;
-		cout << msg;
+		cout << "Introduce el año: ";
 		cin >> anyo;
 	}
+	return anyo;
 }
 
-int solicitaMes(int& mes, string msg) {
-	cout << msg;
+int solicitaMes(int& mes) {
+	cout << "Introduce el mes: ";
 	cin >> mes;
 	while (mes < 1 || mes > 12){
-		cin.fail ();
+		cin.fail();
 		cin.clear();
 		cin.sync();
 		cout << "ERROR: El mes introducido no es válido" << endl;
-		cout << msg;
+		cout << "Introduce el mes: ";
 		cin >> mes;
 	}
+	return mes;
 }
 
-bool esBisiesto (int anyo){
-	return anyo%4==0 && anyo%100!=0 || anyo%400==0;
+bool esBisiesto(int anyo){
+	return anyo % 4 == 0 && anyo % 100 != 0 || anyo % 400 == 0;
 }
 
-int solicitaDia(int anyo, int mes, int& dia, string msg){
-	cout <<msg;
+int solicitaDia(int anyo, int mes, int& dia){
+	cout << "Introduce el dia: ";
 	cin >> dia;
 	if (dia<1 || dia>31){
 		cin.sync();
-		cout <<"ERROR: El dia introducido no es válido" <<endl;
-		solicitaDia(anyo, mes, dia, msg);
+		cout << "ERROR: El dia introducido no es válido" << endl;
+		solicitaDia(anyo, mes, dia);
 	}
-	if ((mes==4 || mes==6 || mes==9 || mes==11) && (dia>30)){
+	if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia>30)){
 		cin.sync();
-		cout <<"ERROR: El dia introducido no es válido" <<endl;
-		solicitaDia(anyo, mes, dia, msg);
+		cout << "ERROR: El dia introducido no es válido" << endl;
+		solicitaDia(anyo, mes, dia);
 	}
-	if (dia>28 && mes==2 && !esBisiesto(anyo)){
+	if (dia>28 && mes == 2 && !esBisiesto(anyo)){
 		cin.sync();
-		cout <<"ERROR: El dia introducido no es válido" <<endl;
-		solicitaDia(anyo, mes, dia, msg);
+		cout << "ERROR: El dia introducido no es válido" << endl;
+		solicitaDia(anyo, mes, dia);
 	}
-	if (dia>29 && mes==2 && esBisiesto(anyo)){
+	if (dia>29 && mes == 2 && esBisiesto(anyo)){
 		cin.sync();
-		cout <<"ERROR: El dia introducido no es válido" <<endl;
-		solicitaDia(anyo, mes, dia, msg);
+		cout << "ERROR: El dia introducido no es válido" << endl;
+		solicitaDia(anyo, mes, dia);
 	}
+	return dia;
 }
 
-int contarBisiestos (int anyoInicio, int anyoFinal, int anyo){ //<-- numero de bisiestos entre 1900--aaaa
+int contarBisiestos(int& anyoInicio, int& anyoFinal, int anyo){ //<-- numero de bisiestos entre 1900--aaaa
 	int numBisiestos;
-	anyoInicio=1900;
-	anyoFinal=anyo;
-	numBisiestos = (anyoFinal-anyoInicio)/4; //<-- Dividir entre 4 pues es el periodo de los bisiestos
+	anyoInicio = 1900;
+	anyoFinal = anyo;
+	numBisiestos = (anyoFinal - anyoInicio) / 4; //<-- Dividir entre 4 pues es el periodo de los bisiestos
 	return numBisiestos;
 }
 
-int diasAnyosCompletos (int anyo, int numBisiestos){ //<-- dias entre 1/1/1900--1/1/aaaa
-	int numAnyos;
-	numAnyos = ((anyo-1900)-numBisiestos)*365+(numBisiestos*366);
+long int diasAnyosCompletos(int anyo, int numBisiestos){ //<-- dias entre 1/1/1900--1/1/aaaa
+	long int numAnyos;
+	numAnyos = ((anyo - 1900) - numBisiestos) * 365 + (numBisiestos * 366);
 	return numAnyos;//<-- Dias transcurridos de aaaa--1900	
 }
 
 
-int dias_transcurridos_en_el_mes_dado(int dia, int mes, int anyo){// <-- Calcula los dias entre el 1--dd
+int dias_transcurridos_en_el_mes_dado(int dia){// <-- Calcula los dias entre el 1--dd
 	int dias_mes;
-	dias_mes = dia-1;
+	dias_mes = dia - 1;
 	return dias_mes; //<-- Dias transcurridos de dd--01 en el mes introducido
 }
 
-int dias_anyo_mas_transcurridos_en_el_mes (int numAnyos, int dias_mes){//<-- Calcula la suma de dias transcurridos en mm y los transcurridos entre 1900-aaaa
+int dias_anyo_mas_transcurridos_en_el_mes(long int numAnyos, int dias_mes){//<-- Calcula la suma de dias transcurridos en mm y los transcurridos entre 1900-aaaa
 	int suma1;
 	suma1 = numAnyos + dias_mes;
 	return suma1;//<-- Dias transcurridos de aaaa--1900+dd--01
@@ -93,71 +96,76 @@ int diferencia_de_meses(int mes){//<-- Calcula el numero de meses entre enero--m
 	return meses;//<-- Numero de meses transcurridos de mm--01
 }
 
+
+int dias[12] = { 0, 31, 29, 31, 30, 31, 30, 30, 31, 30, 31, 30 };
+
 int dias_de_la_diferencia_de_meses(int meses){//<-- Calcula el numero de dias total entre los meses transcurridos hasta enero
-	int diferencia_meses;
-	diferencia_meses=//Expresar en dias los meses transcurridos;
+	int diferencia_meses, n;
+	diferencia_meses = 0;
+	for (n = 0; n <= meses; n++){
+		diferencia_meses = diferencia_meses + dias[n];
+	}
 	return diferencia_meses; //<-- Dias transcurridos de mm--01
 }
 
-int suma_1_mas_dias_transcurridos_entre_meses(int suma1, int mes, int diferencia_meses){//<-- Calcula la suma del metodo anterior mas los dias entre enero--mm	
-	int suma2;
-	suma2=// suma1 (diferencia de años en dias + dias transcurrios al 1 del mes) + diferencia_meses(meses transcurridos hasta enero expresados en dias);
-	return suma2; //<-- Todos los dias transcurridos desde dd/mm/aaaa hasta 01/01/1900
+
+long int diasTranscurridos(int anyo, int suma1, int mes, int diferencia_meses){//<-- Calcula la suma del metodo anterior mas los dias entre enero--mm
+	long int numDias;
+	if (esBisiesto(anyo) && (mes==1 || mes==2)){
+		numDias = (suma1 + diferencia_meses) - 1;
+	}
+	else {
+		numDias = suma1 + diferencia_meses;
+	}
+	return numDias; //<-- Todos los dias transcurridos desde dd/mm/aaaa hasta 01/01/1900
 }
 
-int dia_de_la_semana_en_numero(int suma2){//<-- Calcula el dia de la semana de la fecha introducida
-	int numero_dia_semana;
-	numero_dia_semana = suma2%7;
-	return numero_dia_semana;
+int diaSemana(long int numDias){//<-- Calcula el dia de la semana de la fecha introducida
+	int representacionDia;
+	representacionDia = numDias % 7;
+	return representacionDia;
 }
 
-string nombre_dia_semana(int numero_dia_semana){//<-- Asigna el dia de la semana al numero del metodo anterior
-	string nombre_dia;
-	switch (numero_dia_semana){
-		case 0: nombre_dia = "Lunes"; break;
-		case 1: nombre_dia = "Martes"; break;
-		case 2: nombre_dia = "Miercoles"; break;
-		case 3: nombre_dia = "Jueves"; break;
-		case 4: nombre_dia = "Viernes"; break;
-		case 5: nombre_dia = "Sabado"; break;
-		case 6: nombre_dia = "Domingo"; break;
-	}	
-	return nombre_dia;
+string nombreDiaSemana(int representacionDia){//<-- Asigna el dia de la semana al numero del metodo anterior
+	string nombreDia;
+	switch (representacionDia){
+	case 0: nombreDia = "Lunes"; break;
+	case 1: nombreDia = "Martes"; break;
+	case 2: nombreDia = "Miercoles"; break;
+	case 3: nombreDia = "Jueves"; break;
+	case 4: nombreDia = "Viernes"; break;
+	case 5: nombreDia = "Sabado"; break;
+	case 6: nombreDia = "Domingo"; break;
+	}
+	return nombreDia;
 }
 
-void mostrar(int anyo,int mes,int dia,int numBisiestos,int numAnyos,int dias_mes, int suma1, int meses){
-	cout <<"1-> Año introducido: " <<anyo <<endl;
-	cout <<"2-> Mes introdocido: " <<mes <<endl;
-	cout <<"3-> Dia introducido: " <<dia <<endl;
-	cout <<"4-> Numero de años bisiestos: " <<numBisiestos <<endl;
-	cout <<"5-> Diferencia de años transcurridos exprsado en dias: " <<numAnyos <<endl;
-	cout <<"6-> Dias transcurridos hasta el primer dia del mes " <<mes <<" : " <<dias_mes <<endl;
-	cout <<"7-> Apartado 5 + Apartado 6: "<<numAnyos <<" + " <<dias_mes <<" = " <<suma1 <<endl;
-	cout <<"8-> Meses transcurridos: " <<meses <<endl;
-	cout <<"9-> Diferencia de meses transcurridos expresada en dias: " <<diferencia_meses <<endl;//<--Falta este metodo
-	cout <<"10-> Apartado 7 + Apartado 9: " <<suma1 <<" + " <<diferencia_meses <<" = " <<suma2 <<endl;//<--Falta este metodo
-	cout <<"11-> Numero asociado al dia de la semana: " <<numero_dia_semana <<endl;
-	cout <<"12-> Nombre del dia de la semana: " <<nombre_dia <<endl;
-	cout <<"13-> El " <<dia <<"/" <<mes <<"/" <<anyo <<" es: " <<nombre_dia <<endl;
+void mostrar(string nombreDia, int anyo, int mes, int dia, long int numAnyos, long int numDias){
+	cout <<"1-> Fecha Introducida: " <<dia <<"/"<<mes <<"/" <<anyo <<endl;
+	cout <<"2-> Años transcurridos expresados en dias: " <<numAnyos <<endl;
+	cout <<"3-> Dias transcurridos: " <<numDias <<endl;
+	cout <<"4-> Dia de la semana de la fecha introducida: " <<nombreDia <<endl;
 }
 
-int main (int argc, char** args){
-	int anyo, mes, dia, anyoInicio, anyoFinal, numBisiestos, numAnyos, dias_mes, suma1, meses;
-	int numero_dia_semana, diferencia_meses, suma2;
-	string nombre_dia;
-	solicitaAnyo (anyo, "Introduce el año: ");
-	solicitaMes (mes, "Introduce el mes: ");
-	solicitaDia (anyo, mes, dia, "Introduce el dia: ");
+int main(int argc, char** args){
+	setlocale(LC_ALL, "spanish");
+	int anyo, mes, dia, anyoInicio, anyoFinal, numBisiestos, dias_mes, suma1, meses, diferencia_meses;
+	int representacionDia;
+	long int numAnyos, numDias;
+	string nombreDia;
+	solicitaAnyo(anyo);
+	solicitaMes(mes);
+	solicitaDia(anyo, mes, dia);
 	numBisiestos = contarBisiestos(anyoInicio, anyoFinal, anyo);
-	numAnyos = diasAnyosCompletos (anyo, numBisiestos);
-	dias_mes = dias_transcurridos_en_el_mes_dado(dia, mes, anyo);
-	suma1 = dias_anyo_mas_transcurridos_en_el_mes (numAnyos, dias_mes);
-	meses = diferencia_de_meses (mes);
-	diferencia_meses = dias_de_la_diferencia_de_meses(mes);
-	suma2 = suma_1_mas_dias_transcurridos_entre_meses(suma1, mes, diferencia_meses);
-	numero_dia_semana = dia_de_la_semana_en_numero (suma2);
-	nombre_dia = nombre_dia_semana (numero_dia_semana);
-	mostrar (anyo, mes, dia, numBisiestos, numAnyos, dias_mes, suma1, meses);
-	system ("pause");
+	numAnyos = diasAnyosCompletos(anyo, numBisiestos);
+	dias_mes = dias_transcurridos_en_el_mes_dado(dia);
+	suma1 = dias_anyo_mas_transcurridos_en_el_mes(numAnyos, dias_mes);
+	meses = diferencia_de_meses(mes);
+	diferencia_meses = dias_de_la_diferencia_de_meses(meses);
+	numDias = diasTranscurridos(anyo, suma1, mes, diferencia_meses);
+	representacionDia = diaSemana(numDias);
+	nombreDia = nombreDiaSemana(representacionDia);
+	mostrar(nombreDia, anyo, mes, dia, numAnyos, numDias);
+	system("pause");
 	return 0;
 }
